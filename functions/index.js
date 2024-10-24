680900d2bded40e79ea300ac5505159a67dc3948
Dorfisher <dorfisher95@gmail.com>
2024-10-24 18:26:48 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const { google } = require("googleapis");
-const cors = require("cors")({ origin: true });  // CORS middleware
+const cors = require("cors")({ origin: true });  // Enable CORS for all origins
 
 // Initialize OAuth2 client with Firebase environment variables
 const oAuth2Client = new google.auth.OAuth2(
@@ -11,7 +11,7 @@ const oAuth2Client = new google.auth.OAuth2(
 
 // Cloud Function to list Google Drive files
 exports.listDriveFiles = functions.https.onRequest((req, res) => {
-  cors(req, res, async () => {
+  cors(req, res, async () => {  // Wrap function logic with CORS
     try {
       const { accessToken } = req.query;
 
@@ -19,10 +19,8 @@ exports.listDriveFiles = functions.https.onRequest((req, res) => {
         return res.status(400).send("Access token is required.");
       }
 
-      // Set the OAuth2 client credentials
       oAuth2Client.setCredentials({ access_token: accessToken });
 
-      // Use Google Drive API to list files
       const drive = google.drive({ version: "v3", auth: oAuth2Client });
 
       const response = await drive.files.list({
